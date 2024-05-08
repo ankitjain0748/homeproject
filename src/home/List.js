b56ffcf139c06bf42b80d2769d7e4f03a3106ff9
unknown <ankit.jain0748@gmail.com>
2024-05-08 21:01:38 +0530

    home :

--- src/home/List.js
+++ src/home/List.js
@@ -35,6 +35,7 @@ function List() {
                             <p className="text-sm sm:text-base text-gray-700 dark:text-white">Creating a window to let the world get to know you better.</p>
                         </div>
                     </div>
+                    
                 </div>
 
                 <div className="row grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
